2052014c3efe57eace1f8249c274b7b2bfa65815
Julie Busch <heyjuliebusch@gmail.com>
2016-11-03 15:48:18 -0400

    unfinished Merge Sort

--- mergesort.js
+++ mergesort.js
@@ -12,7 +12,37 @@ function split(wholeArray) {
     firstHalf = wholeArray.slice(0, (size / 2));
     secondHalf = wholeArray.slice(size / 2);
   }
-
-
   return [firstHalf, secondHalf];
+}
+
+function merge(arr1, arr2) {
+  return arr1.concat(arr2);
+}
+
+function mergeSort(array) {
+    var sortedArray = [];
+    //base case
+
+    if(array.length === 1){
+      return array;
+    }
+
+    debugger;
+    //split the array
+    var twoArrays = split(array);
+    var array2 = mergeSort(twoArrays[0]);
+    var array1 = mergeSort(twoArrays[1]);
+
+    //compare
+    if(array2[0] > array1[0]){
+    // sortedArray.push(array2[1][0]);
+    //  sortedArray.push(array1[0][0]);
+      return merge(array1,array2);
+    }else{
+      return array;
+    }
+
+
+
+
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -9,3 +9,32 @@ describe('Split Array function', function() {
     expect(split([1, 2, 3])).toEqual([[1], [2, 3]]);
   });
 });
+describe('Merge Function', function(){
+  it('is able to merge two sorted arrays of different lengths into one sorted array', function(){
+    expect(merge([1], [2, 3])).toEqual([1, 2, 3]);
+  });
+  it('is able to merge two sorted arrays of the same length into one sorted array', function(){
+    expect(merge([1, 2], [3, 4])).toEqual([1, 2, 3, 4]);
+  });
+});
+describe('mergeSort function', function(){
+  beforeEach(function(){
+   spyOn(window, 'mergeSort').and.callThrough();
+  });
+  it('sorts an array with an odd number of things', function(){
+    expect(mergeSort([3, 1, 2])).toEqual([1, 2, 3]);
+  });
+  it('sorts an array with an even number of things', function(){
+    expect(mergeSort([2, 4, 3, 1])).toEqual([1, 2, 3, 4]);
+  });
+  it('handles an array that is already sorted', function(){
+    expect(mergeSort([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
+  });
+  it('handles an array with one thing', function(){
+    expect(mergeSort([2])).toEqual([2]);
+  });
+  it('calls itself', function(){
+    expect(mergeSort.calls.count()).toBeGreaterThan(1);
+  });
+
+});
